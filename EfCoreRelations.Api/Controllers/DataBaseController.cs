d9ad328494104c15f0d4bd01c421ef0a77eb5d91
mahidhar <mahidhar.kumar@gmail.com>
2021-01-26 22:57:01 +0530

    config updated

--- EfCoreRelations.Api/Controllers/DataBaseController.cs
+++ EfCoreRelations.Api/Controllers/DataBaseController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Npgsql;
 using Dapper;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Hosting;
 
 namespace EfCoreRelations.Api.Controllers
 {
@@ -18,11 +19,33 @@ namespace EfCoreRelations.Api.Controllers
 
         private readonly ILogger<DataBaseController> _logger;
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment hostingEnvironment { get; }
 
-        public DataBaseController(ILogger<DataBaseController> logger, IConfiguration configuration)
+        public DataBaseController(ILogger<DataBaseController> logger, IConfiguration configuration, IWebHostEnvironment _hostingEnvironment)
         {
             _logger = logger;
             Configuration = configuration;
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+        private string GetConnectionString(bool isUrl = false)
+        {
+            if (isUrl)
+            {
+                //postgres://username:password@host:port/database
+                string url = Configuration.GetConnectionString("DATABASE_URL");
+
+                string connectionStr = $"Host={url.Split("@")[1].Split(":")[0]};Database={url.Split("@")[1].Split("/")[1]};Username={url.Split("://")[1].Split(":")[0]};Password={url.Split("://")[1].Split(":")[1].Split("@")[0]};Port={url.Split("@")[1].Split(":")[1].Split("/")[0]}";
+
+                if (hostingEnvironment.EnvironmentName == "Production")
+                {
+                    connectionStr = connectionStr + "sslmode=Prefer;Trust Server Certificate=true";
+                }
+
+                return connectionStr;
+            }
+            else
+                return Configuration.GetConnectionString("Dev_BasketballDB");
         }
 
         [HttpGet]
@@ -31,7 +54,7 @@ namespace EfCoreRelations.Api.Controllers
         {
             try
             {
-                using (var connection = new Npgsql.NpgsqlConnection(Configuration.GetConnectionString("Dev_BasketballDB")))
+                using (var connection = new Npgsql.NpgsqlConnection(GetConnectionString()))
                 {
                     return Ok(await connection.QueryAsync<IEnumerable<object>>(query));
                 }
@@ -52,7 +75,7 @@ namespace EfCoreRelations.Api.Controllers
         {
             try
             {
-                using (var connection = new Npgsql.NpgsqlConnection(Configuration.GetConnectionString("Dev_BasketballDB")))
+                using (var connection = new Npgsql.NpgsqlConnection(GetConnectionString()))
                 {
                     var tables = (from row in await connection.QueryAsync(query) select (IDictionary<string, object>)row).AsList();
                     return new JsonResult(tables);
@@ -63,6 +86,28 @@ namespace EfCoreRelations.Api.Controllers
                 return BadRequest(ex.ToString() + ex.StackTrace.ToString());
             }
 
+        }
+
+        [HttpGet]
+        [Route("ConnectionInfo")]
+        public async Task<ActionResult> GetConnectionInfo()
+        {
+            string connectionInfo = "novalue";
+            try
+            {
+                connectionInfo = GetConnectionString(true);
+                using (var connection = new Npgsql.NpgsqlConnection(connectionInfo))
+                {
+                    await connection.OpenAsync();
+                    await connection.CloseAsync();
+
+                    return Ok(connectionInfo + hostingEnvironment.EnvironmentName);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(connectionInfo + hostingEnvironment.EnvironmentName + ex.ToString() + ex.StackTrace.ToString());
+            }
         }
     }
 }
